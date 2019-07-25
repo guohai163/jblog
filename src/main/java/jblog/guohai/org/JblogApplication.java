6888fae26fa5c96248bdc19fa6e651146fd33709
guohai <guohai@gmail.com>
2019-07-25 10:35:49 +0800

    init project

---
+++ src/main/java/jblog/guohai/org/JblogApplication.java
@@ -0,0 +1,13 @@
+package jblog.guohai.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jblog/guohai/org/JblogApplicationTests.java
@@ -0,0 +1,35 @@
+package jblog.guohai.org;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JblogApplicationTests {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void contextLoads() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM blog.gh_posts;");
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+
+		while (resultSet.next()) {
+			String title = resultSet.getString("post_title");
+			System.out.println(title);
+		}
+	}
+
+}
