c4d70de0291e5eb3bf10f7dea90ac4e7d99b0056
DerCgs <mail@changlu.me>
2019-08-09 11:19:49 +0800

    change:去除注释

--- src/main/java/jblog/guohai/org/interceptor/LoginInterceptor.java
+++ src/main/java/jblog/guohai/org/interceptor/LoginInterceptor.java
@@ -14,26 +14,26 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws IOException {
-//        String uuid = null;
-//        if (null == request.getCookies()) {
-//            response.sendRedirect("/admin/");
-//            return false;
-//        }
-//        for (Cookie cookie : request.getCookies()) {
-//            if (cookie.getName().equals("user")) {
-//                uuid = cookie.getValue();
-//                break;
-//            }
-//        }
-//        if (null == uuid) {
-//            response.sendRedirect("/admin/");
-//            return false;
-//        }
-//        UserModel user = UserServiceImpl.getUserByUUID(uuid);
-//        if (null == user) {
-//            response.sendRedirect("/admin/");
-//            return false;
-//        }
+        String uuid = null;
+        if (null == request.getCookies()) {
+            response.sendRedirect("/admin/");
+            return false;
+        }
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals("user")) {
+                uuid = cookie.getValue();
+                break;
+            }
+        }
+        if (null == uuid) {
+            response.sendRedirect("/admin/");
+            return false;
+        }
+        UserModel user = UserServiceImpl.getUserByUUID(uuid);
+        if (null == user) {
+            response.sendRedirect("/admin/");
+            return false;
+        }
         return true;
     }
 }
